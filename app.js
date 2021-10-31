a01dfcf8421484db09729f1f754c5ee5e690a989
sidb2k <49628563+sidb2k@users.noreply.github.com>
2021-10-31 18:41:05 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,110 @@
+
+const express = require("express")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+const ejs = require("ejs")
+
+const app = express()
+app.use(express.static("public"))
+app.set("view engine","ejs")
+app.use(bodyParser.urlencoded({extended:true}))
+
+mongoose.connect('mongodb://localhost:27017/wikiDB', {useNewUrlParser: true, useUnifiedTopology: true})
+
+const articleSchema = {
+  title: String,
+  content: String
+}
+const Article = new mongoose.model("Article",articleSchema)
+
+////////////////////Requesting targeting a specific Articles  ////////////////////////////////////////////////////////
+
+app.route("/articles")
+
+.get(function(req,res){
+    Article.find({},function(err,foundArticles){
+      if(!err)
+      res.send(foundArticles)
+      else
+      res.send(err)
+    })
+})
+
+.post(function(req,res){
+
+  const article = new Article({
+    title: req.body.title,
+    content: req.body.content
+  })
+  article.save(function(err){
+    if(!err)
+    res.send("Inserted")
+    else
+    res.send(err)
+  })
+  })
+
+  .delete(function(req,res){
+    Article.deleteMany(function(err){
+      if(!err)
+      res.send("successfully deleted all articles")
+      else
+      res.send(err)
+    })
+  })
+
+
+////////////////////Requesting targeting all Articles  ////////////////////////////////////////////////////////
+
+app.route("/articles/:articleTitle")
+
+.get(function(req,res){
+  Article.findOne({title:req.params.articleTitle},function(err,foundArticle){
+    if(!err){
+      // console.log(foundArticle);
+      if(foundArticle)
+      res.send(foundArticle)
+      else
+      res.send("Article not found")
+    }
+    else
+      res.send(err)
+  })
+})
+
+.put(function(req,res){
+  Article.updateOne({title: req.params.articleTitle},{title:req.body.title, content: req.body.content},{overwrite: true},function(err){
+    if(!err)
+    res.send("successfully updated article")
+    else
+    res.send(err)
+  })
+})
+
+.patch(function(req,res){
+  // We just pass this JS object inside $set and it will update
+ // req.body={
+ //   title:"something"
+ //   body:"something"
+ // }
+  Article.updateOne({title: req.params.articleTitle},{$set:req.body},function(err){
+    if(!err)
+    res.send("Successfully updated article")
+    else
+    res.send(err)
+  })
+})
+
+.delete(function(req,res){
+  Article.deleteOne({title: req.params.articleTitle},function(err){
+    if(!err)
+    res.send("successfully deleted the article")
+    else
+    res.send(err)
+  })
+});
+
+
+app.listen(3000,function(){
+  console.log("Server started successfully");
+})
